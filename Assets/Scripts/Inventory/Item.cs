75578de3b35c380ad426dc13c71b3698eab53f7d
burakbayboga <burak.bayboga@gmail.com>
2022-05-08 16:07:41 +0300

    Items (Shield + Parrying Dagger)

---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+	public int focusModifier;
+	public int strengthModifier;
+	public int stabilityModifier;
+
+    public Resource resourceModifier;
+	public SkillType modifiedSkillType;
+
+	void Awake()
+	{
+		resourceModifier = new Resource
+		{
+			Focus = focusModifier,
+			Strength = strengthModifier,
+			Stability = stabilityModifier
+		};
+	}
+}
